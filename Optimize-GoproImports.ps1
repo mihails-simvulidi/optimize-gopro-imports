4c4fa8eb8a16f7ee5999586a7ee229c6cfe71def
Mihails Simvulidi <mihails@simvulidi.lv>
2025-08-18 19:37:44 +0300

    Add initial implementation of Optimize-GoproImports script and .gitignore

---
+++ Optimize-GoproImports.ps1
@@ -0,0 +1,139 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$ImportPath,
+
+    [long]$MinimumFreeBytes = 1GB,
+
+    [string]$LogFilePath = "logs$([IO.Path]::DirectorySeparatorChar)"
+)
+
+function Get-HumanReadableSize {
+    param (
+        [Parameter(Mandatory)]
+        [long]$SizeInBytes
+    )
+
+    if ($SizeInBytes -ge 1GB) {
+        return "$([Math]::Round($SizeInBytes / 1GB, 2)) GB"
+    }
+    elseif ($SizeInBytes -ge 1MB) {
+        return "$([Math]::Round($SizeInBytes / 1MB, 2)) MB"
+    }
+    elseif ($SizeInBytes -ge 1KB) {
+        return "$([Math]::Round($SizeInBytes / 1KB, 2)) KB"
+    }
+    else {
+        return "$SizeInBytes B"
+    }
+}
+
+function Get-ScriptLastWriteTime {
+    return (Get-Item $PSCommandPath).LastWriteTime
+}
+
+function Remove-DirectoryIfEmpty {
+    param (
+        [Parameter(Mandatory, ValueFromPipeline = $true)]
+        [System.IO.DirectoryInfo]$Directory
+    )
+    
+    process {
+        $normalizedDirectoryPath = $Directory.FullName.TrimEnd([IO.Path]::DirectorySeparatorChar)
+        if ($normalizedDirectoryPath.Length -gt $normalizedImportPath.Length -and
+            $normalizedDirectoryPath.StartsWith($normalizedImportPath, [StringComparison]::OrdinalIgnoreCase) -and
+            -not (Get-ChildItem -Force -LiteralPath $Directory)
+        ) {
+            Write-Log "Deleting empty directory $Directory..."
+            Remove-Item -Force -LiteralPath $Directory.FullName
+        }
+    }
+}
+
+function Remove-File {
+    param (
+        [Parameter(Mandatory, ValueFromPipeline = $true)]
+        [System.IO.FileInfo]$File
+    )
+
+    process {
+        Write-Log "Deleting file $File ($(Get-HumanReadableSize -SizeInBytes $File.Length))..."
+        Remove-Item -Force -LiteralPath $File.FullName
+    }
+}
+
+function Write-DriveFreeSpace {
+    param (
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSDriveInfo]$Drive
+    )
+
+    Write-Log "Free space on drive ${Drive}: $(Get-HumanReadableSize -SizeInBytes $Drive.Free)"
+}
+
+function Write-Log {
+    param (
+        [string]$Message
+    )
+
+    Write-Host "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff") $Message"
+}
+
+Start-Transcript -Append -LiteralPath "$LogFilePath$(Get-Date -Format "yyyy-MM-dd_HH-mm").log"
+
+try {
+    $normalizedImportPath = (Get-Item $ImportPath).FullName.TrimEnd([IO.Path]::DirectorySeparatorChar)
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.IncludeSubdirectories = $true
+    $watcher.Path = $ImportPath
+
+    Register-ObjectEvent -InputObject $watcher -EventName Created -Action {
+        Write-Log "Directory or file creation detected: $($Event.SourceEventArgs.FullPath)"
+        if ($Event.SourceEventArgs.Name -ilike $duplicateFilter -or (Split-Path -Extension -Path $Event.SourceEventArgs.Name) -iin '.LRV', '.THM') {
+            Remove-File -ErrorAction SilentlyContinue -File $Event.SourceEventArgs.FullPath
+        }
+    } | Out-Null
+
+    Register-ObjectEvent -InputObject $watcher -EventName Deleted -Action {
+        Write-Log "Directory or file deletion detected: $($Event.SourceEventArgs.FullPath)"
+        Split-Path -Path $Event.SourceEventArgs.FullPath | Get-Item | Remove-DirectoryIfEmpty
+    } | Out-Null
+
+    $watcher.EnableRaisingEvents = $true
+
+    Get-ChildItem -File -Filter *.LRV -Force -LiteralPath $ImportPath -Recurse | Remove-File
+    Get-ChildItem -File -Filter *.THM -Force -LiteralPath $ImportPath -Recurse | Remove-File
+
+    $duplicateFilter = '* (*).*'
+    Get-ChildItem -File -Filter $duplicateFilter -Force -LiteralPath $ImportPath -Recurse | Remove-File
+
+    Get-ChildItem -Directory -Force -LiteralPath $ImportPath -Recurse | Remove-DirectoryIfEmpty
+
+    $drive = (Get-Item $ImportPath).PSDrive
+    $scriptLastWriteTime = Get-ScriptLastWriteTime
+
+    while ($true) {
+        if ($drive.Free -lt $MinimumFreeBytes) {
+            Write-DriveFreeSpace -Drive $drive
+            $files = Get-ChildItem -File -Force -LiteralPath $ImportPath -Recurse | Sort-Object LastWriteTime
+            foreach ($file in $files) {
+                Remove-File -File $file
+                if ($drive.Free -ge $MinimumFreeBytes) {
+                    break
+                }
+            }
+            Write-DriveFreeSpace -Drive $drive
+        }
+
+        # Postpone execution till the next minute so that the we can check for script modifications right before the next scheduled run
+        $currentTime = Get-Date
+        Start-Sleep -Milliseconds (60000 - $currentTime.Second * 1000 - $currentTime.Millisecond)
+
+        if ((Get-ScriptLastWriteTime) -gt $scriptLastWriteTime) {
+            Write-Log "Script has been modified — exiting."
+            break
+        }
+    }
+}
+finally {
+    Stop-Transcript
+}
